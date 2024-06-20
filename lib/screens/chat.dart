85c9a447bce2ff4213d92ec809377f19bb924115
Johannes Krabbe <github@Krabbe.dev>
2024-06-20 21:05:13 +0200

    enhance layout

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -15,126 +15,100 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  reverse: true,
-                  children: [
-                    AnimatedSize(
-                      curve: Curves.easeIn,
-                      duration: const Duration(milliseconds: 100),
-                      child: SizedBox(
-                          height:
-                              renderedHeight < 20 ? 20 : renderedHeight - 20),
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello',
-                    ),
-                    const Message(
-                      isMe: false,
-                      message:
-                          'Hellosdf asd asd;lfkHello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello ',
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
-                    ),
-                    const Message(
-                      isMe: false,
-                      message:
-                          'Hellosdf asd asd;lfkHello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello ',
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello',
-                    ),
-                    const Message(
-                      isMe: false,
-                      message:
-                          'Hellosdf asd asd;lfkHello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello ',
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
-                    ),
-                    const Message(
-                      isMe: false,
-                      message:
-                          'Hellosdf asd asd;lfkHello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello ',
-                    ),
-                    const Message(
-                      isMe: true,
-                      message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
-                    ),
-                    const SizedBox(height: 250),
-                  ],
-                ),
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
               ),
-            ],
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:
-                        Theme.of(context).colorScheme.surface.withOpacity(0.5),
-                  ),
-                  child: const ChatHead(),
-                ),
-              ),
+              child: const ChatHead(),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:
-                        Theme.of(context).colorScheme.surface.withOpacity(0.5),
-                  ),
-                  child: AnimatedSize(
-                    curve: Curves.easeIn,
-                    duration: const Duration(milliseconds: 100),
-                    child: Builder(
-                      builder: (context) {
-                        return NotificationListener(
-                          onNotification: (notification) {
-                            if (notification is ScrollMetricsNotification) {
-                              if (context.size!.height != renderedHeight) {
-                                setState(() {
-                                  renderedHeight = context.size!.height;
-                                });
-                              }
-                            }
-                            return true;
-                          },
-                          child: const ChatInput(),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
+        ),
+      ),
+      body: ListView(
+        reverse: true,
+        children: const [
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
+          ),
+          Message(
+            isMe: true,
+            message: 'Hello Hello Hello Hello Hello HelloHello aaaaa',
           ),
         ],
+      ),
+      bottomNavigationBar: AnimatedSize(
+        curve: Curves.easeIn,
+        duration: const Duration(milliseconds: 100),
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+            child: Builder(
+              builder: (context) {
+                return NotificationListener(
+                  onNotification: (notification) {
+                    if (notification is ScrollMetricsNotification) {
+                      if (context.size!.height != renderedHeight) {
+                        setState(() {
+                          renderedHeight = context.size!.height;
+                        });
+                      }
+                    }
+                    return true;
+                  },
+                  child: const ChatInput(),
+                );
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -266,7 +240,7 @@ class ChatHead extends StatelessWidget {
               const CircleAvatar(
                 radius: 20,
                 backgroundImage: NetworkImage(
-                    'https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b'),
+                    'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png'),
               ),
               const SizedBox(width: 10),
               const Expanded(
--- lib/screens/chat_list.dart
+++ lib/screens/chat_list.dart
@@ -13,78 +13,66 @@ class _ChatListState extends State<ChatList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-            // todo: add sized boxed to top and bottom of list.
-          ListView(
-            children: chatListItems,
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:
-                        Theme.of(context).colorScheme.surface.withOpacity(0.5),
-                  ),
-                  child: Column(
-                    children: [
-                      SafeArea(
-                        bottom: false,
-                        child: Text(
-                          "Chats",
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSurface,
-                            fontSize: 24,
-                          ),
-                        ),
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
+              ),
+              child: Column(
+                children: [
+                  SafeArea(
+                    bottom: false,
+                    child: Text(
+                      "Chats",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontSize: 24,
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
           ),
-          Positioned(
-            bottom: -10,
-            left: 0,
-            right: 0,
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:
-                        Theme.of(context).colorScheme.surface.withOpacity(0.5),
-                  ),
-                  child: BottomNavigationBar(
-                    currentIndex: 0,
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    items: const <BottomNavigationBarItem>[
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.chat),
-                        label: 'Chat',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.people_alt),
-                        label: 'Contacts',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.settings),
-                        label: 'Settings',
-                      ),
-                    ],
-                  ),
+        ),
+      ),
+      body: ListView(
+        children: chatListItems,
+      ),
+      bottomNavigationBar: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
+            ),
+            child: BottomNavigationBar(
+              currentIndex: 0,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.chat),
+                  label: 'Chat',
                 ),
-              ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people_alt),
+                  label: 'Contacts',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: 'Settings',
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
@@ -97,83 +85,83 @@ var chatListItems = const <Widget>[
         "Hello, how are you? Hello, how are you? Hello, how are you? Hello, how are you? Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   ),
   ChatTile(
     name: "John Doe",
     message: "Hello, how are you?",
     time: "15:30",
     avatarUrl:
-        "https://storage.googleapis.com/pfpai/styles/574c62e5-0c3d-4e31-b49d-d9a4c8b07bc9.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production@stockai-362303.iam.gserviceaccount.com/20240606/auto/storage/goog4_request&X-Goog-Date=20240606T020118Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=99b7440c3cdb369961dd20b15c8dc4d280a7ceb91ea24ab153b08a7938e03c35da316f860c2a37cf94ec35b1b4d00db8cb34869b18b06e9d6618c5a0bed62371c9ff6e9253f1284ba5f624e1451b3ca1edc25e3b217e98df33dd904525397f7fec7c4fc6c0bbeb8f5d2aa85a006602df4098681c45f80e427929f895d5a8c7b84353ba81550516068058e50171106c9931becf448109f7dd979f8a96e5eb78a3f1fb227d8965f44bc28add8ee4dc54fe6dc9c730e931d13629b1134879e130d9f7a011c66b1259306abbc1bc43cf27d5690be63b60902aa3605ecd7eb7d3850f15c683ba8b1e27b49251707980eaba3ae710650ce124be1c3e761e823cd7a83b",
+        "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
   )
 ];
