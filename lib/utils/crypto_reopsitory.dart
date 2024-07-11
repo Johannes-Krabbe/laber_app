08f6dc487556cc662872f0847a7ece633f954d86
Johannes Krabbe <github@Krabbe.dev>
2024-07-11 15:49:25 +0200

    continue work on crypto repo

--- lib/utils/crypto_reopsitory.dart
+++ lib/utils/crypto_reopsitory.dart
@@ -1,6 +1,8 @@
 import 'package:cryptography/cryptography.dart';
 import 'package:laber_app/utils/curve/ed25519_util.dart';
+import 'package:laber_app/utils/id_generator.dart';
 import 'package:laber_app/utils/secure_storage_repository.dart';
+import 'dart:convert';
 
 class CryptoRepository {
   final secureStorage = SecureStorageRepository();
@@ -18,31 +20,44 @@ class CryptoRepository {
     }
   }
 
-  Future<SimpleKeyPair> getSignedPreKeyPairs() async {
+  Future<List<SignedPreKeyPair>> getSignedPreKeyPairs() async {
     const name = 'signedPreKeyPair';
     final signedPreKeyPairString = await secureStorage.read(name);
 
     if (signedPreKeyPairString != null) {
-      return Ed25519Util.stringToKeyPair(signedPreKeyPairString);
+      var keypairs = json.decode(signedPreKeyPairString);
+      List<SignedPreKeyPair> signedPreKeyPairs = [];
+      for (var keypair in keypairs) {
+        signedPreKeyPairs.add(await SignedPreKeyPair.fromJson(keypair));
+      }
+      return signedPreKeyPairs;
     } else {
       final keyPair = await Ed25519Util.generateKeyPair();
-      secureStorage.write(name, await Ed25519Util.keyPairToString(keyPair));
-      return keyPair;
+      return [SignedPreKeyPair(keyPair, newCuid())];
     }
+  }
+
+  Future<List<SignedPreKeyPair>> addSignedPreKeyPairs(int count) async {
+    List<SignedPreKeyPair> signedPreKeyPairs = [];
+    for (var i = 0; i < count; i++) {
+      final keyPair = await Ed25519Util.generateKeyPair();
+      signedPreKeyPairs.add(SignedPreKeyPair(keyPair, newCuid()));
+    }
+    return signedPreKeyPairs;
   }
 }
 
 class SignedPreKeyPair {
   final SimpleKeyPair keyPair;
   DateTime timestamp;
-  String? id;
+  String id;
 
   SignedPreKeyPair(this.keyPair, this.id, {DateTime? timestamp})
       : timestamp = timestamp ?? DateTime.now();
 
   setId(String id) => {this.id = id};
 
-  Future<SignedPreKeyPair> fromJson(Map<String, dynamic> json) async {
+  static Future<SignedPreKeyPair> fromJson(Map<String, dynamic> json) async {
     final keyPair = await Ed25519Util.stringToKeyPair(json['keyPair']);
     final id = json['id'];
     final timestamp = DateTime.parse(json['timestamp']);
