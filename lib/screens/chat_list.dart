4791b03617aab06930db6ed56b1db5fcd805657f
Johannes Krabbe <github@Krabbe.dev>
2024-07-18 19:07:15 +0200

    sliver app bar with blur backgound

--- lib/screens/chat_list.dart
+++ lib/screens/chat_list.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:laber_app/components/chat_tile.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
@@ -11,11 +12,36 @@ class ChatList extends StatefulWidget {
 }
 
 class _ChatListState extends State<ChatList> {
+  final List<Widget> chatWidgets = getFakeWidgets();
+
+  late ScrollController _scrollController;
+  Color _textColor = Colors.white;
+  static const kExpandedHeight = 200.0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _textColor = _isSliverAppBarExpanded ? Colors.white : Colors.black;
+        });
+      });
+  }
+
+  bool get _isSliverAppBarExpanded {
+    return _scrollController.hasClients &&
+        _scrollController.offset > kExpandedHeight - kToolbarHeight;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
+      /*
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
         child: ClipRect(
@@ -44,8 +70,47 @@ class _ChatListState extends State<ChatList> {
           ),
         ),
       ),
-      body: ListView(
-        children: getFakeWidgets(),
+      */
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            snap: false,
+            floating: false,
+            expandedHeight: 100.0,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            forceMaterialTransparency: true,
+            centerTitle: false,
+            flexibleSpace: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+                child: FlexibleSpaceBar(
+                  titlePadding: const EdgeInsets.all(8.0),
+                  title: Text("Chats",
+                      style: TextStyle(
+                        color: _textColor,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(
+              bottom: 100,
+            ),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return chatWidgets[index];
+                },
+                childCount: chatWidgets.length,
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: ClipRect(
         child: BackdropFilter(
@@ -90,9 +155,53 @@ Function() getFakeWidgets = () {
         message: faker.lorem.sentence(),
         time: faker.date.justTime(),
         avatarUrl:
-            "https://randomuser.me/api/portraits/med/men/${i.toString()}.jpg" ,
+            "https://randomuser.me/api/portraits/med/men/${i.toString()}.jpg",
       ),
     );
   }
   return chatListItems;
 };
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight > minHeight ? maxHeight : minHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
+
+class SearchWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSearchTextField(
+      placeholder: "Search",
+      backgroundColor: Colors.grey,
+      onChanged: (value) {
+        print(value);
+      },
+    );
+  }
+}
